3e874d73509102558f1aeac499c04e9300b8fe86
ahmadyosr <ahmadyosr@gmail.com>
2018-03-05 21:14:19 +0200

    recovered dashboard views

--- umrahgo/settings.py
+++ umrahgo/settings.py
@@ -62,6 +62,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'catalogue',
     'customer',
+    'dashboard',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
