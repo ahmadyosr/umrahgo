43307b140186697d97fefcece71d8f7211ee0a59
ahmadyosr <ahmadyosr@gmail.com>
2018-03-08 16:41:07 +0200

    omg

--- catalogue/models.py
+++ catalogue/models.py
@@ -55,6 +55,7 @@ class Package(models.Model):
 	quin_room_cost = models.IntegerField(default = 0 )
 
 
+	airlines = models.CharField(max_length =100) # choices are('BUS' , 'FLIGHT')
 	transport = models.CharField(max_length =10 , default ="BUS" ) # choices are('BUS' , 'FLIGHT')
 	month = models.IntegerField(default =0 )
 	year = models.IntegerField(default =0 )
