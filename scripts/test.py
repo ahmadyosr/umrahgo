fc142ef5fb3c6362c9f6c49d376d5ffd43e450f6
ahmadyosr <ahmadyosr@gmail.com>
2018-03-09 17:02:46 +0200

    omg

--- scripts/test.py
+++
@@ -1,7 +0,0 @@
-from catalogue.models import Package , Agency , MakkahHotel , MadinahHotel 
-import random
-def run():
-
-	for i in range(5) : 
-		MakkahHotel.objects.create(title = 'ahmad' ) 
-		MadinahHotel.objects.create(title = 'ahmad' ) 
